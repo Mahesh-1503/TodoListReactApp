4af1ec7abef615cbecb22443910fc3ff9e886a7d
Mahesh-1503 <mnopxyz090@gmail.com>
2024-09-02 12:29:48 +0530

    resolving errors

--- responsive-task-manager/src/Header.js
+++ responsive-task-manager/src/Header.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Header({ title }) {
+function Header({ title = 'Task Manager' }) {
   return (
     <header>
       <h1>{title}</h1>
     </header>
   );
 }
-
-// Default props
-Header.defaultProps = {
-  title: 'Task Manager'
-};
 
 // Prop types
 Header.propTypes = {
